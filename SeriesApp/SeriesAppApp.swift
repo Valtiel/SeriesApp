31c96c4165879e27c6f558fdb7a6f77d6207904c
Cesar Rosales <augusto.valtiel@gmail.com>
2023-09-04 12:01:12 -0300

    Initial Commit

---
+++ SeriesApp/SeriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeriesAppApp.swift
+//  SeriesApp
+//
+//  Created by César Rosales on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SeriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
